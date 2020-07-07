d1859d526e902f1c65ee43880ab6b11c7e066c29
Shakti-Prasad-Satapathy <53331332+Shakti-Prasad-Satapathy@users.noreply.github.com>
2020-07-07 10:11:17 +0530

    Update carrierwave.rb
    Hide te aws seckret and access kye

--- Fundo/config/initializers/carrierwave.rb
+++ Fundo/config/initializers/carrierwave.rb
@@ -4,9 +4,9 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
         provider:               'AWS',
-        aws_access_key_id:      ENV['AKIAISF4I4QS3TLV6JSA'],
-        aws_secret_access_key:  ENV['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'],
-        region:                 'ap-south-1',
+        aws_access_key_id:      ENV['****************'],
+        aws_secret_access_key:  ENV['******************************'],
+        region:                 '*********-********************',
     }
 =begin
     if Rails.env.test? || Rails.env.cucumber?
@@ -19,7 +19,7 @@ CarrierWave.configure do |config|
 =end
 
     config.cache_dir = "#{Rails.root}/tmp/uploads"
-    config.fog_directory = ENV['fundoapp']
+    config.fog_directory = ENV['********']
     case Rails.env
         when 'production'
         config.fog_directory = 'dummy'
